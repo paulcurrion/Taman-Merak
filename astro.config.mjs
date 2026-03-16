15ca32bc974996ee43ea341647235327be399969
paulcurrion <paulcurrion@hotmail.com>
2026-03-16 14:13:30 +0100

    Fix Astro setup for GitHub pages, fix gallery images, cleanup Netlify cruft

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,15 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
     // 1. Add your GitHub Pages URLs here
-  site: 'https://tamanmerak.com',
+    site: 'https://tamanmerak.com',
 
     // 2. Keep your Tailwind and React configs
     vite: {
         plugins: [tailwindcss()]
     },
     integrations: [react()]
-    
-    // Notice: The Netlify adapter is completely gone!
-})
+});
